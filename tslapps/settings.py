df8af1ad9fa3c924210dbd734810459669943f67
Biswajee <roy.biswajeet161@gmail.com>
2019-08-12 10:55:39 +0530

    initial commit (secured)

--- tslapps/settings.py
+++ tslapps/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 import json
-with open('../secrets.json') as f:
+with open('secrets.json') as f:
   data = json.load(f)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = data.SECRET_KEY
+SECRET_KEY = data["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
