2b5f2c460cf7e4669a45f2b9b646b5260c461261
Biswajee <roy.biswajeet161@gmail.com>
2019-08-14 03:33:12 +0530

    Code refractor

--- tslapps/urls.py
+++ tslapps/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include('dashboard.urls')),
-    path(r'api/', include('automationAPI.urls')),
+    # path(r'', include('dashboard.urls')),
+    path(r'', include('automationAPI.urls')),
 ]
