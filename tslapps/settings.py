f7e06fd917517de7eb64a03dfa2aaa83f16fd810
Biswajee <roy.biswajeet161@gmail.com>
2019-08-14 03:52:35 +0530

    Implement rest framework TokenAuthentication

--- tslapps/settings.py
+++ tslapps/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'rest_framework',
+    'rest_framework.authtoken',
     'automationAPI'
 ]
 
@@ -129,8 +130,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-# Pagination
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
